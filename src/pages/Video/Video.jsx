1e62ff891532b660b27f97734cbec3ca2ddee88b
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-02-01 11:42:28 +0100

    added the pages and componenets

---
+++ src/pages/Video/Video.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Video.css";
+
+const Video = () => {
+  return (
+    <div>
+      <h1>Welcome to TubeYuu Video Page!</h1>
+    </div>
+  );
+};
+export default Video;
